b67f5ccf33723caca63fe2e8579ee6705867d32c
salvacarrion <salva.carrion@outlook.com>
2023-01-20 13:44:56 +0100

    Major improvements

--- README.md
+++ README.md
@@ -1,24 +1,61 @@
 # FindWords
-Find words
+
+Find words in a dictionary, following a pattern.
+The goal of this project is to find mnemotecnic rules easily to remember words, sentences, or even numbers.
 
 ## Usage
 
+There are 2 modes, with 2* options each:
+- **Sequential:** Find words that match your pattern in order: (zpt → "...z...p...t...")
+  - **With extra vowels:** (zpt → zapata, zapatear, zapato,...)
+  - **With NO extra vowels:** Checks if the word exists
+- **Permutation**: Find anagrams for your pattern: (zpt → zpt, ztp, pzt, ptz, tzp, tpz)
+  - **With extra vowels:** (zpt → zapato, topaz, potazo,...)
+  - **With NO extra vowels:** Checks if your word is an anagram of another. (mrao -> roma, mora, amor, ramo,...)
+
+> **PRO TIP:** The **most useful modes** are:
+> - **Sequential with extra vowels:** To find words matching the pattern
+> - **Permutation with NO extra vowels:** To find anagrams of the pattern
+
+
+###  Examples: Sequential + Extra vowels
 ```python
 $python find_words.py
 Loading words...
 Building dataset...
-¿Ignorar todas las vocales? (y/n): n
-¿Preservar el orden de las letras? (y/n): n
-Introduce las letras a buscar: asdu
-asiduo
-osuda
-saudí
-suda
-sudá
-usad
-usado
+Preservar el orden de las letras? (y/n) [n]: >? y
+Se pueden añadir vocales adicionales? (y/n) [n]: >? y
+Introduce las letras a buscar: >? zptd
+zapatead
+zapateado
+zapateador
+zapateadora
+zapatuda
+zapatudo
 --------------------------------------------------
-- Palabra buscada: asdu
-- Total palabras encontradas: 7
-- Tiempo de ejecución: 1.074 segundos
+- Patrón buscado: zptd
+- Total palabras encontradas: 6
+- Tiempo de ejecución: 0.562 segundos
+```
+
+
+###  Examples: Anagram + With NO extra vowels
+```python
+Loading words...
+Building dataset...
+Preservar el orden de las letras? (y/n) [n]: >? 
+Se pueden añadir vocales adicionales? (y/n) [n]: >? 
+Introduce las letras a buscar: >? mrao
+amor
+armo
+armó
+maro
+mora
+morá
+ramo
+roma
+--------------------------------------------------
+- Patrón buscado: mrao
+- Total palabras encontradas: 8
+- Tiempo de ejecución: 0.356 segundos
 ```
--- find_words.py
+++ find_words.py
@@ -1,5 +1,13 @@
 import time
 import os
+import re
+from collections import OrderedDict
+
+# Check if requests package is installed, else raise an error
+try:
+    import requests
+except ImportError:
+    raise ImportError('The requests package is not installed. Please install it with "pip install requests".')
 
 URL_DICTIONARY = "https://github.com/JorgeDuenasLerin/diccionario-espanol-txt/raw/master/0_palabras_todas.txt"
 
@@ -25,17 +33,18 @@ def remove_accents(word):
         'Ü': 'U',
         'Ñ': 'N'
     }
-    
+
     # Create a list of the letters of the word
     letters = list(word)
-    
+
     # Replace the letters with accents with the letters without accents
     for i in range(len(letters)):
         if letters[i] in accents_dictionary:
             letters[i] = accents_dictionary[letters[i]]
-    
+
     # Return the word without accents
     return ''.join(letters)
+
 
 def remove_diacritics(word_without_accents):
     """
@@ -43,16 +52,17 @@ def remove_diacritics(word_without_accents):
     """
     # Create a list of the letters of the word
     letters = list(word_without_accents)
-    
+
     # Replace the letters with diacritics with the letters without diacritics
     for i in range(len(letters)):
         if letters[i] == '¿':
             letters[i] = 's'
         elif letters[i] == '¡':
             letters[i] = 'i'
-    
+
     # Return the word without diacritics
     return ''.join(letters)
+
 
 def remove_vowels(word_without_accents):
     """
@@ -60,14 +70,15 @@ def remove_vowels(word_without_accents):
     """
     # Create a list of the letters of the word
     letters = list(word_without_accents)
-    
+
     # Replace the vowels with the letters without vowels
     for i in range(len(letters)):
         if letters[i] in 'aeiouAEIOU':
             letters[i] = ''
-    
+
     # Return the word without vowels
     return ''.join(letters)
+
 
 def get_missing_vowels(word):
     vowels = 'aeiou'
@@ -78,97 +89,114 @@ def get_missing_vowels(word):
     missing_vowels = set(missing_vowels)
     return missing_vowels
 
+
 def load_words(file_name):
     # Open the file and load the words into a list
     with open(file_name) as file:
         words = file.read().split()
     return words
 
-def build_dataset(words):
+
+def build_dataset(word_list):
     # Create a list of words without accents, diacritics and the given vowels
     words_without_accents = []
-    for word in words:
-        word_cleaned = word.lower().strip()
+    for w in word_list:
+        word_cleaned = w.lower().strip()
         word_cleaned = remove_accents(word_cleaned)
         word_cleaned = remove_diacritics(word_cleaned)
 
         # Add cleaned word to list
         words_without_accents.append(word_cleaned)
-    
+
     # Create a dictionary that maps words without accents to a list of the words that have them
     words_dictionary = {}
     for i in range(len(words_without_accents)):
         if words_without_accents[i] in words_dictionary:
-            words_dictionary[words_without_accents[i]].append(words[i])
+            words_dictionary[words_without_accents[i]].append(word_list[i])
         else:
-            words_dictionary[words_without_accents[i]] = [words[i]]
+            words_dictionary[words_without_accents[i]] = [word_list[i]]
     return words_dictionary
-        
-def find_words(letters, words_dictionary, ignore_all_vowels=False, preserve_order=False):
-    # Make letters lowercase, remove accents and diacritics
-    letters = letters.lower().strip()
-    letters = remove_accents(letters)
-    letters = remove_diacritics(letters)
 
-    # Get missing vowels
-    if ignore_all_vowels:
-        vowels2remove = set('aeiou')
-    else:
-        vowels2remove = get_missing_vowels(letters)
 
-    # Find all words in the dictionary whose key is a permutation of the given letters
+def find_words(pattern, words_dictionary, preserve_order=False, allow_extra_vowels=False):
+    # Clean pattern
+    pattern = pattern.lower().strip()
+    pattern = remove_accents(pattern)
+    pattern = remove_diacritics(pattern)
+
+    # Build pattern to ignore vowels
+    new_pattern = ''
+    if allow_extra_vowels:
+        # Create a regular pattern where the missing vowels are optional
+        for letter in pattern:
+            new_pattern += '[aeiou]*' + letter
+        new_pattern += '[aeiou]*'
+
+    # Find words that match the pattern
     matched_words = []
-    for key in words_dictionary:
-        # Remove vowels from key (keeping order of the chars)
-        new_key = [char for char in key if char not in vowels2remove]
-        new_key = ''.join(new_key)
+    for clean_word in words_dictionary:
+        if preserve_order:
+            if allow_extra_vowels:  # Useful when the pattern has no vowels, else, tricky
+                # Check if the clean word matches the pattern
+                if re.match(new_pattern, clean_word):
+                    # Add the words to the tmp ist
+                    matched_words += words_dictionary[clean_word]
 
-        # Check if the new_key is a permutation of the given letters
-        word = words_dictionary[key]
-        if preserve_order and (new_key == letters):
-            matched_words += word
-        elif not preserve_order and (sorted(new_key) == sorted(letters)):
-            matched_words += word
-        else:
-            pass
-    
+            else:  # Dummy case
+                if clean_word == pattern:
+                    matched_words += words_dictionary[clean_word]
+
+        else:  # Permutations
+            if allow_extra_vowels: # Not very useful
+                missing_vowels = get_missing_vowels(pattern)
+
+                # Remove the missing vowels from the clean word
+                tmp_clean_word = [letter for letter in clean_word if letter not in missing_vowels]
+
+                # Check if both words contain the same letters (excluding the missing vowels)
+                if set(tmp_clean_word) == set(pattern):
+                    matched_words += words_dictionary[clean_word]
+            else:  # Definetly useful
+                if sorted(clean_word) == sorted(pattern):
+                    matched_words += words_dictionary[clean_word]
     return matched_words
 
-def download_file(url, path):    
+
+def download_file(url, path):
     # Download file form URL
-    import requests
     r = requests.get(url)
 
     # Save file
     with open(path, 'wb') as file:
-        file.write(r.content)  
+        file.write(r.content)
+
 
 if __name__ == '__main__':
     print("Loading words...")
 
     # Check if the dictionary exists, else, download it
-    file_name = os.path.join(os.path.dirname(__file__), 'words.txt')
+    file_name = os.path.join(os.path.dirname(__file__), 'data/words.txt')
     if not os.path.exists(file_name):
         print("Dictionary not found. Downloading dictionary...")
         download_file(URL_DICTIONARY, file_name)
 
     # Load words
-    words = load_words(file_name=file_name)
+    word_list = load_words(file_name=file_name)
 
     print("Building dataset...")
-    words_dictionary = build_dataset(words=words)
-    
-    # Ask if vowels should be ignore
-    ignore_all_vowels = input('¿Ignorar todas las vocales? (y/n): ').lower().strip() == 'y'
-    preserve_order = input('¿Preservar el orden de las letras? (y/n): ').lower().strip() == 'y'
+    words_dictionary = build_dataset(word_list=word_list)
+
+    # Ask for settings (with default value)
+    preserve_order = (input("Preservar el orden de las letras? (y/n) [n]: ").lower().strip() or 'n') == 'y'
+    allow_extra_vowels = (input("Se pueden añadir vocales adicionales? (y/n) [n]: ").lower().strip() or 'n') == 'y'
 
     # Get the letters from the user
     while True:
-        letters = input('Introduce las letras a buscar: ')
+        pattern = input('Introduce las letras a buscar: ')
 
         # Find words
         start_time = time.time()
-        matched_words = find_words(letters, words_dictionary, ignore_all_vowels, preserve_order)
+        matched_words = find_words(pattern, words_dictionary, preserve_order, allow_extra_vowels)
         end_time = time.time()
 
         # Print the list of words (one per line)
@@ -177,9 +205,7 @@ if __name__ == '__main__':
 
         # Summary
         print('-' * 50)
-        print(f'- Palabra buscada: {letters}')
+        print(f'- Patrón buscado: {pattern}')
         print(f'- Total palabras encontradas: {len(matched_words)}')
         print(f'- Tiempo de ejecución: {end_time - start_time:.3f} segundos')
         print('')
-
-    
